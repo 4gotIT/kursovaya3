0eb1736f148c848d4e385def0dc40761509b222b
Aziz <ibodov.aziz@yandex.ru>
2023-11-03 16:10:53 +0300

    add test fuction sorted_list

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,8 +4,8 @@ from utils.funcs import*
 
 
 def test_load_data():
-    assert load_data([]) is None, 'проверка на другие типы'
-    assert load_data(12) is None, 'проверка на другие данные'
+    assert load_data([]) is None, 'проверка на другие типы данных'
+    assert load_data(12) is None, 'проверка на другие типы данных'
     assert load_data('') is None, 'проверка на неверный путь'
     assert load_data() is None, "проверка на пустой аргумент"
     assert load_data('../operations.json')[0] == {
@@ -24,5 +24,40 @@ def test_load_data():
     "to": "Счет 64686473678894779589"
   } , 'проверяем верно ли считывает данные из файла'
 
-# def test_sorted_list():
+def test_sorted_list():
+    test_data_1 = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
+    test_data_2 = {
+        "id": 441945886,
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }
+    assert sorted_list() == [], 'проверка на пустой аргумент'
+    assert sorted_list(2) == [], 'при других типах, возвращает пустой список'
+    assert sorted_list(load_data('../operations.json')[0]) == [test_data_1], 'проверка с ключом'
+    assert sorted_list(test_data_2) == [], 'проверка без ключа, возвращает пустой список'
+    assert len(sorted_list(load_data('../operations.json'))) == 85, 'количество элементов с ключом == 85'
+
 
