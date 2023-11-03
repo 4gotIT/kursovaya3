d18f8f27dc3c3cbe1e7c36e88c125c731cd500b5
Aziz <ibodov.aziz@yandex.ru>
2023-11-03 15:25:59 +0300

    test_load_data function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,25 @@
 import os
 
-import pytest
-from utils.funcs import load_data, sorted_list
+from utils.funcs import*
 
 
 def test_load_data():
-    assert load_data('') is None
-    assert load_data('path') is None
+    assert load_data('') is None, 'проверка на неверный путь'
+    assert load_data() is None, "проверка на пустой аргумент"
+    assert load_data('../operations.json')[0] == {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  } , 'проверяем верно ли считывает данные из файла'
 
-def test_sorted_list():
+
