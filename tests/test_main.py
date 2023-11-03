cc413f073e20e94175ca34c6db1205dadc32b5cf
Aziz <ibodov.aziz@yandex.ru>
2023-11-03 15:30:59 +0300

    add some tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,8 @@ from utils.funcs import*
 
 
 def test_load_data():
+    assert load_data([]) is None, 'проверка на другие типы'
+    assert load_data(12) is None, 'проверка на другие данные'
     assert load_data('') is None, 'проверка на неверный путь'
     assert load_data() is None, "проверка на пустой аргумент"
     assert load_data('../operations.json')[0] == {
@@ -22,4 +24,5 @@ def test_load_data():
     "to": "Счет 64686473678894779589"
   } , 'проверяем верно ли считывает данные из файла'
 
+# def test_sorted_list():
 
